871d68bcf4a6833a773e663908fb6f2070752ffa
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2021-04-22 12:32:06 -0400

    Bump dependencies, chisel-circt 0.2.0
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
-  lazy val circt = "com.sifive" %% "chisel-circt" % "0.1.0"
-  lazy val chisel = "edu.berkeley.cs" %% "chisel3" % "3.4.1"
-  lazy val chiselTest = "edu.berkeley.cs" %% "chiseltest" % "0.3.1"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.7"
+  lazy val circt = "com.sifive" %% "chisel-circt" % "0.2.0"
+  lazy val chisel = "edu.berkeley.cs" %% "chisel3" % "3.4.3"
+  lazy val chiselTest = "edu.berkeley.cs" %% "chiseltest" % "0.3.3"
 }
