21981ff07b7476b00feeeb2b388fe8fc37865834
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2022-02-19 17:03:05 -0500

    Bump all dependencies to latest
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.7"
-  lazy val circt = "com.sifive" %% "chisel-circt" % "0.2.0"
-  lazy val chisel = "edu.berkeley.cs" %% "chisel3" % "3.4.3"
-  lazy val chiselTest = "edu.berkeley.cs" %% "chiseltest" % "0.3.3"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
+  lazy val circt = "com.sifive" %% "chisel-circt" % "0.3.0"
+  lazy val chisel = "edu.berkeley.cs" %% "chisel3" % "3.5.1"
+  lazy val chiselTest = "edu.berkeley.cs" %% "chiseltest" % "0.5.1"
 }
