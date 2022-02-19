sifive/chisel-circt-demo

commit b2142fd1f38f6526e49a04a65fbeee0aa96939f0

Demonstrate annotation passing in example
Add a dontTouch'd wire to the example Chisel circuit in order to
demonstrate that certain annotation-backed Chisel APIs work with CIRCT.

Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

src/main/scala/example/Foo.scala
@@ -7,7 +7,10 @@ class Foo extends MultiIOModule {
   val a = IO(Input(Bool()))
   val b = IO(Output(Bool()))
 
-  b := ~a
+  val w = WireDefault(~a)
+  dontTouch(w)
+
+  b := a
 }
 
 object Main extends App {

commit bd3808c7d26176b8f475619388045a5b0d19a6e4

Fix bug in Hello circuit
Fix bug introduced in b2142fd where the output was changed from
inverting the input to not inverting the input.

Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

src/main/scala/example/Foo.scala
@@ -10,7 +10,7 @@ class Foo extends MultiIOModule {
   val w = WireDefault(~a)
   dontTouch(w)
 
-  b := a
+  b := w
 }
 
 object Main extends App {